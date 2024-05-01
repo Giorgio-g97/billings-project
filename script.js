ea5316b2f1638a361752731a63f698be77c9ca6a
Giorgio-g97 <giorgio.g97@gmail.com>
2024-05-02 00:25:28 +0200

    initializing project

---
+++ script.js
@@ -0,0 +1,73 @@
+let insegnanti = [
+    {
+        nome: "Roberta",
+        cognome: "Rosiello",
+        img: "https://amamb.it/wp-content/uploads/2021/03/Roberta-Rosiello-Amamb.png",
+        citta: "Foggia",
+        telefono: "",
+        email: "",
+    },
+    {
+        nome: "Cristina",
+        cognome: "Paveri",
+        img: "",
+        citta: "Galatone (LE)",
+        telefono: "",
+        email: "",
+    },
+    {
+        nome: "Francesca",
+        cognome: "Iuspa",
+        img: "",
+        citta: "Foggia",
+        telefono: "",
+        email: "",
+    },
+    {
+        nome: "Roberta",
+        cognome: "Rosiello",
+        img: "",
+        citta: "Foggia",
+        telefono: "",
+        email: "",
+    },
+    {
+        nome: "Roberta",
+        cognome: "Rosiello",
+        img: "",
+        citta: "Foggia",
+        telefono: "",
+        email: "",
+    },
+]
+
+const elencoIns = document.querySelector('.elenco-insegnanti')
+
+/* forEach insegnanti */
+
+insegnanti.forEach(el => {
+    let insegnHTML = `
+    <div class="bg-slate-200 w-[200px] mx-auto p-5 rounded-xl my-5 flex flex-col items-center gap-3">
+        <img src=${el.img} />
+        <div class="flex gap-1" >
+        <h2 class="font-bold">${el.nome}</h2>
+        <h2 class="">${el.cognome}</h2>
+        </div>
+        <h2>${el.citta}</h2>
+        <h2>${el.telefono}</h2>
+    </div>
+        `
+    elencoIns.innerHTML += insegnHTML
+});
+
+/* Bottone copia CF */
+
+const copyBtn = document.querySelector('.copy-btn')
+const input = document.querySelector('.input-cf')
+copyBtn.addEventListener('click', ()=> {
+    navigator.clipboard.writeText(input.value)
+    copyBtn.textContent = 'Copiato!'
+    setTimeout(() => {
+        copyBtn.textContent = 'Copia'
+    }, 3000);
+})
