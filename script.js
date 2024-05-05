012b110645ae50dc853acb82eeee0b216e826c4c
Giorgio-g97 <giorgio.g97@gmail.com>
2024-05-05 13:33:25 +0200

    Aggiustati gli avatar insegnanti

--- script.js
+++ script.js
@@ -4,40 +4,56 @@ let insegnanti = [
         cognome: "Rosiello",
         img: "https://amamb.it/wp-content/uploads/2021/03/Roberta-Rosiello-Amamb.png",
         citta: "Foggia",
-        telefono: "",
-        email: "",
+        telefono: "348 9168294",
+        email: "robertarosiellocopy@gmail.com",
     },
     {
         nome: "Cristina",
         cognome: "Paveri",
         img: "",
         citta: "Galatone (LE)",
-        telefono: "",
-        email: "",
+        telefono: "380 1572879",
+        email: "cristinapaveri@alice.it",
+    },
+    {
+        nome: "Franca",
+        cognome: "Zavatta",
+        img: "",
+        citta: "Ascoli Satriano(FG)",
+        telefono: "389 1815693",
+        email: "francesca.zavatta@virgilio.it",
+    },
+    {
+        nome: "Mariacarla",
+        cognome: "Cosenza",
+        img: "",
+        citta: "Foggia",
+        telefono: "349 5927878",
+        email: "mariacarlacosenza@tiscali.it",
     },
     {
         nome: "Francesca",
         cognome: "Iuspa",
         img: "",
         citta: "Foggia",
-        telefono: "",
-        email: "",
+        telefono: "347 2450015",
+        email: "francesca.iuspa@tiscali.it",
     },
     {
-        nome: "Roberta",
-        cognome: "Rosiello",
+        nome: "Lucia",
+        cognome: "Miglionico",
         img: "",
-        citta: "Foggia",
-        telefono: "",
-        email: "",
+        citta: "San Giovanni Rotondo (FG)",
+        telefono: "339 8720137",
+        email: "lucia.miglionico82@gmail.com",
     },
     {
-        nome: "Roberta",
-        cognome: "Rosiello",
+        nome: "Maria Gabriella",
+        cognome: "Turrin",
         img: "",
-        citta: "Foggia",
-        telefono: "",
-        email: "",
+        citta: "Deliceto (FG)",
+        telefono: "3479851524",
+        email: "m.gabriella@yahoo.it",
     },
 ]
 
@@ -46,9 +62,12 @@ const elencoIns = document.querySelector('.elenco-insegnanti')
 /* forEach insegnanti */
 
 insegnanti.forEach(el => {
+    if(el.img === ""){
+        el.img = "https://t3.ftcdn.net/jpg/03/53/11/00/360_F_353110097_nbpmfn9iHlxef4EDIhXB1tdTD0lcWhG9.jpg"
+    }
     let insegnHTML = `
     <div class="bg-slate-200 w-[200px] mx-auto p-5 rounded-xl my-5 flex flex-col items-center gap-3">
-        <img src=${el.img} />
+        <img class="rounded-full" src=${el.img} />
         <div class="flex gap-1" >
         <h2 class="font-bold">${el.nome}</h2>
         <h2 class="">${el.cognome}</h2>
@@ -69,7 +88,7 @@ copyBtn.addEventListener('click', ()=> {
     copyBtn.textContent = 'Copiato!'
     setTimeout(() => {
         copyBtn.textContent = 'Copia'
-    }, 3000);
+    }, 1500);
 })
 
 /* Gestione chiusura menù */
